9ed10553f1edd630c47f582807479a0ecfae2489
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-04-22 19:01:59 +0200

    install React and confirgure dependencies

---
+++ sportsee/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>salut les potes </p>
+      
+    </div>
+  );
+}
+
+export default App;
