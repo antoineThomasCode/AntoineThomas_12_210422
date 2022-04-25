4308fe8ae0ee450cec0419062b90ca68fb96d1f0
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-04-25 13:05:11 +0200

    added chart

--- sportsee/src/pages/profil.jsx
+++ sportsee/src/pages/profil.jsx
@@ -2,23 +2,18 @@ import React from "react";
 import "../scss/pages/profil.scss"
 
 import BarChartUser from "../components/BarChart";
+import AverageSession from "../components/AverageChart";
 
 function Profil ({userInfos, userActivity, userPerformance, userAverageSession}) {
     const currentUserInfo = userInfos.find(user => user.id === 12)
     const currentUserActivity = userActivity.find(user => user.userId === 12)
-    console.log(currentUserActivity.sessions)
     
 
     return (
         <main className="main-profil">
            <h1>Bonjour  <span className="last-name"> {currentUserInfo.userInfos.firstName}</span> </h1>
            <BarChartUser className="daily-activity-graph" data={currentUserActivity.sessions}/>
-           <div className="chart-container">
-                <p>autre graph </p>
-                <p>autre graph </p>
-                <p>autre graph </p>
-                <p>autre graph </p>
-           </div>
+           <AverageSession className='average-session-graph'/>
         </main>
     )
 }
