577ee574567667552bca34f47e35be92a1cd2ce5
Antoine Thomas <antoinethomas.devweb@gmail.com>
2022-05-15 14:46:05 +0200

    added hook in profil.jsx for Api calls

--- sportsee/src/pages/profile.jsx
+++ sportsee/src/pages/profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import genericFetch from "../utils/genericFetch";
 import "../scss/pages/profile.scss"
 
 import BarChartUser from "../components/BarChart";
@@ -16,6 +15,7 @@ import fire from '../assets/images/fire-icon.png'
 import chicken from '../assets/images/chicken-icon.png'
 import burger from '../assets/images/burger-icon.png'
 import apple from '../assets/images/apple-icon.png'
+import requestHandler from "../utils/genericFetch";
 
 
 
@@ -27,39 +27,65 @@ function Profile ({userInfos, userActivity, userPerformance, userAverageSession}
 
     const  { id } = useParams();
     const currentUserInfo = userInfos.find(user => user.id === parseInt(id))
-    const currentUserActivity = userActivity.find(user => user.userId === parseInt(id))
+    /*const currentUserActivity = userActivity.find(user => user.userId === parseInt(id))*/
     const currentUserAverageSession = userAverageSession.find(user => user.userId === parseInt(id))
-    const currentUserPerformances = userPerformance.find(user => user.userId === parseInt(id))
+    const currentUserPerformances = userPerformance.find(user => user.userId === parseInt(id)) 
     
-    const [dataState, setDataState] = useState({})
+    const [dataUser, setDataUser] = useState()
+    const [userActivitySessions, setUserActivitySessions] = useState() 
+    const [userPerformanceData, setUserPerformanceData] = useState()
+    const [userAverageSessionsData, setAverageSessionsData] = useState()
 
     useEffect(() => {
       const getApi = async () => {
-        const data = await genericFetch(parseInt(id), 'activity');
-        setDataState(data);
+        const data = await requestHandler({url: `http://localhost:3000/user/${id}/`});
+        setDataUser(data.data);
       };
       getApi();
-    }, [])
-    console.log(dataState.data) 
-   
-    return (
-        <main className="main-profil">
-           <h1>Bonjour  <span className="last-name"> {currentUserInfo.userInfos.firstName}</span> </h1>
-           <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-           <div className="charts-container">
-                <BarChartUser className="daily-activity-graph" data={currentUserActivity.sessions}/>
-                <AverageSession className='average-session-graph' session={currentUserAverageSession.sessions}/>
-                <Performance data={currentUserPerformances.data} />
-                <ScoreChart userScore={currentUserInfo.score} color={'red'} />
-           </div>
-           <div className="logo-list">
-                <InfoCard icon={fire} alt={'icon des calories'} quantity={currentUserInfo.keyData.calorieCount} unity={'kCal'} nutrient={'Calories'}/>
-                <InfoCard icon={chicken} alt={'icon des protéines'} quantity={currentUserInfo.keyData.proteinCount} unity={'g'} nutrient={'Proteines'}/>
-                <InfoCard icon={apple} alt={'icon des Glucides'} quantity={currentUserInfo.keyData.carbohydrateCount} unity={'g'} nutrient={'Glucides'}/>
-                <InfoCard icon={burger} alt={'icon des Lipides'} quantity={currentUserInfo.keyData.lipidCount} unity={'g'} nutrient={'Lipides'}/>
-           </div>
-        </main>
-    )
+    }, [id])
+
+    useEffect(() => {
+      const getApi = async () => {
+        const data = await requestHandler({url: `http://localhost:3000/user/${id}/activity`});
+        setUserActivitySessions(data.data);
+      };
+      getApi();
+    }, [id])
+
+    useEffect(() => {
+      const getApi = async () => {
+        const data = await requestHandler({url: `http://localhost:3000/user/${id}/performance`});
+        setUserPerformanceData(data.data);
+      };
+      getApi();
+    }, [id])
+
+    useEffect(() => {
+      const getApi = async () => {
+        const data = await requestHandler({url: `http://localhost:3000/user/${id}/average-sessions`});
+        setAverageSessionsData(data.data);
+      };
+      getApi();
+    }, [id])
+
+    if (!dataUser || !userActivitySessions || !userPerformanceData || ! userAverageSessionsData) {
+      return <main className="main-profil"><div>Chargement des données de l'utilisateur</div></main>
+    } return <main className="main-profil">
+                <h1>Bonjour  <span className="last-name"> {dataUser ? (dataUser.userInfos.firstName) : 'data non chargé'}</span> </h1>
+                <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                <div className="charts-container">
+                      <BarChartUser className="daily-activity-graph" data={userActivitySessions ?  (userActivitySessions.sessions) : 'data'}/>
+                      <AverageSession className='average-session-graph' session={userAverageSessionsData.sessions}/>
+                      <Performance data={userPerformanceData.data} />
+                      <ScoreChart userScore={dataUser.score} color={'red'} />
+                </div>
+                <div className="logo-list">
+                      <InfoCard icon={fire} alt={'icon des calories'} quantity={dataUser.keyData.calorieCount} unity={'kCal'} nutrient={'Calories'}/>
+                      <InfoCard icon={chicken} alt={'icon des protéines'} quantity={dataUser.keyData.proteinCount} unity={'g'} nutrient={'Proteines'}/>
+                      <InfoCard icon={apple} alt={'icon des Glucides'} quantity={dataUser.keyData.carbohydrateCount} unity={'g'} nutrient={'Glucides'}/>
+                      <InfoCard icon={burger} alt={'icon des Lipides'} quantity={dataUser.keyData.lipidCount} unity={'g'} nutrient={'Lipides'}/>
+                </div>
+            </main>
 }
 
 export default Profile
--- sportsee/src/utils/genericFetch.jsx
+++ sportsee/src/utils/genericFetch.jsx
@@ -1,6 +1,23 @@
-const genericFetch = async (userId, endPoint ) => {
-    const data = await fetch(`http://localhost:3000/user/${userId}/${endPoint}`);
+/*const genericFetch = async (endPoint, method = GET ) => {
+    const data = await fetch(`http://localhost:3000/user/${endPoint}`);
     const result = data.json()
     return result
 }
-export default genericFetch
+export default genericFetch */
+
+const requestHandler = async ({
+    url,
+    method = 'GET',
+    headers = {},
+    body = '',
+    errMsg = 'Failed to fetch data',
+  }) => {
+    try {
+      const res = await fetch(url, { method, headers, body: body === '' ? null : body });
+      return res.json();
+    } catch (err) {
+        console.log(err)
+    }
+  };
+  
+  export default requestHandler;
